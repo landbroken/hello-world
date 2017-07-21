a88f48ece80a7d809b6e5e61db17c794f3e8df70
lyl <445299258@qq.com>
2017-07-21 17:03:54 +0800

    hello world C# code

--- HelloWorld/Form1.cs
+++ HelloWorld/Form1.cs
@@ -16,5 +16,10 @@ namespace HelloWorld
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Hello World!";
+        }
     }
 }
